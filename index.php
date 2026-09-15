8324ce2190bfdfcfd0143602ca8d6dd603478ef3
Mina Remseth <mina@remseth.com>
2026-09-15 14:26:51 +0200

    Opprettet index.php for test

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="no">
+<head>
+    <meta charset="UTF-8">
+    <title>Kundeservice / Ticketsystem</title>
+</head>
+<body>
+    <h1>Velkommen til Ticketsystemet</h1>
+    <p>Dette er en test</p>
+</body>
+</html>
