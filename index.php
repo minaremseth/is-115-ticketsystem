50ad4944c68101114782a5be7b411dd1abe661b2
Mina Remseth <mina@remseth.com>
2026-09-15 15:41:33 +0200

    Laget registrering

--- index.php
+++ index.php
@@ -5,7 +5,27 @@
     <title>Kundeservice / Ticketsystem</title>
 </head>
 <body>
-    <h1>Velkommen til Ticketsystemet</h1>
-    <p>Dette er en test</p>
+    <h1>Send inn en hendvendelse</h1>
+    <form action="index.php" method="POST" enctype="multipart/form-data">
+        Navn:<br>
+        <input type="text" name="navn" required><br>
+        Epost:<br>
+        <input type="email" name="epost" required><br>
+        Kategori: <br>
+        <select id="kategori" name="kategori" required>
+            <option value="">--Velg kategori-- </option>
+            <option value="teknisk">Teknisk støtte </option>
+            <option value="faktura">Betaling og faktura </option>
+            <option value="generelt">Generelt </option>
+        </select><br>
+        Emne: <br>
+        <input type="text" name="emne" required><br>
+        Beskrivelse: <br>
+        <textarea name="beskrivelse" rows="5"></textarea><br>
+        Vedlegg: <br>
+        <input type="file" name="vedlegg"><br>
+        <button type="submit" name="submit">Send inn hendvendelse</button>
+
+    </form>
 </body>
 </html>
